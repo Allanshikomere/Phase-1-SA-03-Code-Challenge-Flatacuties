5935df99d44395767ded03e98d2151248d31659d
Allanshikomere <allanshikomere@gmail.com>
2023-12-28 23:20:57 +0300

    Updated

--- script.js
+++ script.js
@@ -1,63 +1,64 @@
-// Fetch the data from the server
+// Fetch the JSON data
 fetch('db.json')
-    .then(response => response.json())
-    .then(data => {
-        const charactersData = data.characters;
+  .then(response => response.json())
+  .then(data => renderCharacters(data.characters))
+  .catch(error => console.error('Error fetching data:', error));
 
-        // Function to create a character card
-        function createCharacterCard(character) {
-            const card = document.createElement('article');
-            card.classList.add('character-card');
+// Renders the characters to the DOM
+function renderCharacters(characters) {
+  const list = document.getElementById('charactersList');
+  
+  
+  characters.forEach(character => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+      <div class="container">
+        <img src="${character.image}" alt="${character.name}">
+        <span>${character.name}</span>
+      </div>
+      <div class="buttons">
+        <button class="vote-btn" data-id="${character.id}">Vote</button>
+        <button class="reset-btn" data-id="${character.id}">Reset</button>
+      </div>
+      <div></div>
+      <div></div>
+    `;
+    list.appendChild(listItem);
+  
+    // Attach click event to display character details
+    listItem.addEventListener('click', () => displayCharacterDetails(character));
+  });
+  
 
-            const img = document.createElement('img');
-            img.src = character.image;
-            img.alt = character.name;
-            card.appendChild(img);
+  // Add event listeners after appending elements
+  list.addEventListener('click', function(event) {
+    if (event.target.classList.contains('vote-btn')) {
+      addVote(Number(event.target.dataset.id)); // Convert to number if necessary
+    }
+    if (event.target.classList.contains('reset-btn')) {
+      resetVotes(Number(event.target.dataset.id)); // Convert to number if necessary
+    }
+  });
+  listItem.addEventListener('click', () => displayAnimalDetails(character));
 
-            const name = document.createElement('h2');
-            name.textContent = character.name;
-            name.dataset.characterId = character.id; // Assign the character's ID as a data attribute
-            card.appendChild(name);
+}
 
-            const votes = document.createElement('p');
-            votes.textContent = `Votes: ${character.votes}`;
-            card.appendChild(votes);
+function displayCharacterDetails(character) {
+  const detailsContainer = document.getElementById('characterDetails');
+  
+  detailsContainer.innerHTML = '';
+  
+  const name = document.createElement('h2');
+  name.textContent = character.name;
+  
+  const image = document.createElement('img');
+  image.src = character.image;
+  image.alt = character.name;
 
-            return card;
-        }
+  const votes = document.createElement('p'); 
+  votes.textContent = `Votes: ${character.votes}`;
 
-        // Function to render characters
-        function renderCharacters(characters) {
-            const container = document.getElementById('characters-container');
-            characters.forEach(character => {
-                const card = createCharacterCard(character);
-                container.appendChild(card);
-            });
-        }
-
-        // Function to display details of a specific character
-        function displayCharacterDetails(character) {
-            const detailsDiv = document.getElementById('animal-details');
-            detailsDiv.innerHTML = `
-                <h2>${character.name}</h2>
-                <img src="${character.image}" alt="${character.name}">
-                <p>Votes: ${character.votes}</p>
-            `;
-        }
-
-        // Event listener for character clicks
-        const container = document.getElementById('characters-container');
-        container.addEventListener('click', event => {
-            if (event.target.tagName === 'H2') {
-                // Get the character ID from the data attribute
-                const characterId = event.target.dataset.characterId;
-                // Find the character object in the charactersData array
-                const character = charactersData.find(char => char.id === characterId);
-                // Display the character details
-                displayCharacterDetails(character);
-            }
-        });
-
-        // Render the characters initially
-        renderCharacters(charactersData);
-    });
+  detailsContainer.appendChild(name);
+  detailsContainer.appendChild(image);
+  detailsContainer.appendChild(votes);
+}
